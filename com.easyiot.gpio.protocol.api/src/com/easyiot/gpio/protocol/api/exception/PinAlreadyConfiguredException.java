1cfbb9f625241d405d14c920a44b11a7c938fd17
daghan <daghanacay@gmail.com>
2016-09-15 21:09:59 +1000

    update exception and release

--- com.easyiot.gpio.protocol.api/src/com/easyiot/gpio/protocol/api/exception/PinAlreadyConfiguredException.java
+++ com.easyiot.gpio.protocol.api/src/com/easyiot/gpio/protocol/api/exception/PinAlreadyConfiguredException.java
@@ -1,12 +1,14 @@
 package com.easyiot.gpio.protocol.api.exception;
 
+import com.easyiot.base.api.exception.FrameworkException;
+
 /**
  * Thrown if an already configured pin is attempted to be reconfigured
  * 
  * @author daghan
  *
  */
-public class PinAlreadyConfiguredException extends Exception {
+public class PinAlreadyConfiguredException extends FrameworkException {
 	private static final long serialVersionUID = 1L;
 
 }
