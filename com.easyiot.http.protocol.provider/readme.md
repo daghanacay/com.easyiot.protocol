7a07a069efd1ae5877ad08669547cdd0fb7c94ac
daghan <daghanacay@gmail.com>
2016-11-10 21:58:10 +1100

    changing to mvn

---
+++ com.easyiot.http.protocol.provider/readme.md
@@ -0,0 +1,15 @@
+# 
+
+${Bundle-Description}
+
+## Example
+
+## Configuration
+
+	Pid: com.easyiot.http.protocol
+	
+	Field					Type				Description
+		
+	
+## References
+
---
+++ com.easyiot.http.protocol.provider/src/com/easyiot/http/protocol/provider/ProtocolImpl.java
@@ -0,0 +1,11 @@
+package com.easyiot.http.protocol.provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * 
+ */
+@Component(name = "com.easyiot.http.protocol")
+public class ProtocolImpl {
+
+}
