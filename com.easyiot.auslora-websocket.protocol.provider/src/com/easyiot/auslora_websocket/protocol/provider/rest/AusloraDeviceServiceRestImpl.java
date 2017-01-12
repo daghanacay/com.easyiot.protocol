b519d6d7f7bfb0e289aa6516ca453a726ea37a5b
daghan <daghanacay@gmail.com>
2017-01-12 21:57:41 +1100

    working rest service

--- com.easyiot.auslora-websocket.protocol.provider/src/com/easyiot/auslora_websocket/protocol/provider/rest/AusloraDeviceServiceRestImpl.java
+++ com.easyiot.auslora-websocket.protocol.provider/src/com/easyiot/auslora_websocket/protocol/provider/rest/AusloraDeviceServiceRestImpl.java
@@ -2,7 +2,6 @@ package com.easyiot.auslora_websocket.protocol.provider.rest;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
 import com.easyiot.auslora_websocket.protocol.api.AusloraDeviceService;
 import com.easyiot.auslora_websocket.protocol.api.dto.AusloraDeviceListDto;
@@ -10,8 +9,7 @@ import com.easyiot.auslora_websocket.protocol.api.dto.AusloraDeviceListDto;
 import osgi.enroute.rest.api.REST;
 import osgi.enroute.rest.api.RESTRequest;
 
-@Component(immediate = true, property = {
-		HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN + "=/auslora/application/*" })
+@Component(immediate = true)
 public class AusloraDeviceServiceRestImpl implements REST {
 	@Reference
 	AusloraDeviceService ausloraDeviceService;
